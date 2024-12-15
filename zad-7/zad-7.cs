d357ffa3e45e1174a01c4e9ab928f86d7f9f63fa
Nataliq-Vulkova <nataliavalkova2009@gmail.com>
2024-12-15 19:12:08 +0200

    Add files via upload

---
+++ zad-7/zad-7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n= ");
+            int n= int.Parse(Console.ReadLine());
+            var p = true;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+                if (n % i == 0)
+                {
+                    p = false; break;
+                }
+            if (p)
+            
+                Console.WriteLine("Prosto");
+            
+            else
+                Console.WriteLine("Slojno");
+        }
+    }
+}
